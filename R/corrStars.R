959c9e7bcfe95318a0668a2bdc320062a48c7124
Andrew Taylor <taylor.andrew.r@gmail.com>
2016-09-01 07:15:23 -0400

    Added the ability to print Ns for each correaltion into the table.

--- R/corrStars.R
+++ R/corrStars.R
@@ -5,16 +5,18 @@
 #' @param x Numeric matrix/data.frame
 #' @param method correlation method: spearman, pearson, kendall
 #' @param dec number of decimal places for correlation coefficients
+#' @param N Logical, whether or not to include the N for each correlation
 #'
 #' @return data.frame containing correlation table
 #' @export
 #'
 #' @examples
 #' #NULL
-corrStars <- function(x, method = "spearman", dec = 2) {
+corrStars <- function(x, method = "spearman", dec = 2, N = FALSE) {
   x <- as.matrix(x)
   R <- rcorr(x,type = method)$r
   p <- rcorr(x,type = method)$P
+  N <- rcorr(x, type = method)$n
   ## define notions for significance levels; spacing is important.
   mystars <-
     ifelse(p <= .001, "***", ifelse(p <= .01, "** ", ifelse(p <= .05, "*", " ")))
@@ -27,12 +29,19 @@ corrStars <- function(x, method = "spearman", dec = 2) {
   diag(Rnew) <- paste(diag(R), " ", sep = "")
   rownames(Rnew) <- colnames(x)
   colnames(Rnew) <- paste(colnames(x), "", sep = "")
+  #Add pairwise Ns if N = TRUE
+  if(N = TRUE) {
+    Rnew <- matrix(paste(N, Rnew, sep = ", "), ncol = ncol(Rnew))
+  }
   ## remove upper triangle
   Rnew <- as.matrix(Rnew)
   Rnew[upper.tri(Rnew, diag = TRUE)] <- ""
   Rnew <- as.data.frame(Rnew)
   ## remove last column and return the matrix (which is now a data frame)
-  Rnew <- cbind(Rnew[1:length(Rnew) - 1])
+  Rnew <- Rnew[, -length(colnames(x))]
+  Rnew <- Rnew[-1,]
+  colnames(Rnew) <- colnames(x)[1:(length(colnames(x))-1)]
+  rownames(Rnew) <- colnames(x)[2:length(colnames(x))]
   print("Note: ***: p \u2264 0.001; ** : p \u2264 0.01; *: p \u2264 0.05, no asterisk: p > 0.05")
   return(Rnew)
 }

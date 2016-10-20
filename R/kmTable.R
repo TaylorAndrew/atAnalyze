a318a297db41f97a7de81409c63658f6677c76b1
Andrew Taylor <taylor.andrew.r@gmail.com>
2016-10-20 12:25:38 -0400

    Strata names now added to kmTable output

--- R/kmTable.R
+++ R/kmTable.R
@@ -73,6 +73,7 @@ kmTable <- function(data, time, event, group) {
   outputAsMatrix[c(1:length(pairwise[,1])),6] <- pairwise[,2]
   outputAsMatrix[1, 7] <- diffP
   output <- as.data.frame(outputAsMatrix)
+  output[1:length(names(fit$strata)),1] <- gsub( ".*=", "", names(fit$strata))
   names(output) <-
     c(
       "Group", "N Total", "N Events" ,"Median Survival (95% CI)",
